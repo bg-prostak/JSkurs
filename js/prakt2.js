3a90088cefc4bcad8196e0f69cd4095ff4f4d600
bg-prostak <bgprostaktwd@gmail.com>
2022-07-18 17:35:27 +0300

    практическое задание 2

---
+++ js/prakt2.js
@@ -0,0 +1,41 @@
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+'use strict';
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?' , '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {} ,
+    genres: [],
+    privat: false
+};
+for (let i =0; i<numberOfFilms; i++ ){
+    const film = prompt('Какой фильм вы посмотрели?' , '') 
+    if (film.length>=10 || film.length == null ){
+        console.log("Введите корректное название");
+        i--;
+        continue;
+        
+    }
+    const number = prompt('На сколько оцените его?', '')
+    personalMovieDB.movies[film]=number;
+}
+if (personalMovieDB.count < 10){
+    console.log("Мало");
+} else {if(personalMovieDB.count < 30){
+    console.log("Средне");
+}else {console.log("Много");}}
+
+console.log(personalMovieDB);
